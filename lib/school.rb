b0fba44ba8c7a0781d4293497ee6e1f41a996e2e
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-02-11 18:52:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,6 +19,39 @@ class School
         return y 
       end 
     end 
+  end 
+  
+# This works, but it isn't passing learn.co's tests cause of 
+# how it displays syntactically...but the same information is there
+#  def sort 
+#    roster.sort_by do |x, y| 
+#      x 
+#    end 
+#  end 
+
+# Here's learn.co's solution:
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
 end 
-end
-# code here!
+  
+end 
+
+
+
+school = School.new("Bayside High School")
+school.roster
+school.add_student("Zach Morris", 9)
+school.add_student("Moshe Zeus", 9)
+school.add_student("Onkelos Octavius", 9)
+school.roster
+school.add_student("Sugar Zaza", 5)
+school.roster
+school.add_student("Theo Madus", 5)
+school.add_student("Abbah Zabbah", 2)
+school.roster
+school.grade(9)
+school.sort
